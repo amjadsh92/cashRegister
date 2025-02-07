960877a1a9fb7bcb7d973ff7dcadee284cc6ce15
amjadsh92 <amjadsharafeddien@gmail.com>
2025-02-07 08:28:19 +0200

    change and clean the structure of the code

--- cashRegister.js
+++ cashRegister.js
@@ -23,10 +23,19 @@ const power = {
   "ONE HUNDRED": 100,
 };
 
+const priceElement = document.getElementById("price");
+const output = document.getElementById("output");
+const listOfNotes = document.getElementById("list-of-notes");
+const cash = document.getElementById("cash-value");
+const purchaseButton = document.getElementById("purchase");
+priceElement.innerText = price;
 
-const changeStatus = (cashPaid) => {
-  let cid_test = cid.map((item) => [...item]);
-  let amountPaid = {
+const has_change = (cashPaid) => {
+  //create a deepcopy of the original cid
+  let cid_temp = cid.map((item) => [...item]);
+
+  //amount of money returned per each note to complete the change due
+  let amountOfNoteReturned = {
     PENNY: 0,
     NICKEL: 0,
     DIME: 0,
@@ -37,100 +46,105 @@ const changeStatus = (cashPaid) => {
     TWENTY: 0,
     "ONE HUNDRED": 0,
   };
-  let stat = "";
-  let result = { stat, amountPaid };
+  // status of the transcation: Is there a change due
+  //  or not and the status of the change due, if there is
+  let status = "";
+  // info is the whole information of the transaction:
+  // status of change + amount of money returned per note
+  let info = { status, amountOfNoteReturned };
 
+  let change = Number((cashPaid - price).toFixed(2));
 
-  const returnChange = (cashPaid) => {
-    let remainder = Number((cashPaid - price).toFixed(2));
+  const canReturnChange = (change) => {
+    for (let noteInfo of cid_temp) {
+      let note = noteInfo[0];
+      let noteAmount = noteInfo[1];
 
-    for (let cash of cid_test) {
-      if (remainder % power[cash[0]] === 0 && cash[1] >= remainder) {
-        cash[1] = Number((cash[1] - remainder).toFixed(2));
-        amountPaid[cash[0]] += Number(remainder.toFixed(2));
-        if(cid_test.every((cash)=> cash[1] === 0)){
-            result.stat="CLOSED"
+      if (!(change % power[note]) && noteAmount >= change) {
+        noteAmount = Number((noteAmount - change).toFixed(2));
+        noteInfo[1] = noteAmount;
+        amountOfNoteReturned[note] += Number(change.toFixed(2));
+
+        if (cid_temp.every((cash) => cash[1] === 0)) {
+          info.status = "CLOSED";
+        } else {
+          info.status = "OPEN";
         }
-        else{
-            result.stat = "OPEN";
-        }
-       
-        cid = [...cid_test];
 
-        return result;
+        cid = [...cid_temp];
+        return info;
       }
     }
+  };
+  const checkForChange = (change) => {
+    for (let i = cid_temp.length - 1; i >= 0; i--) {
+      let note = cid_temp[i][0];
+      let noteAmount = cid_temp[i][1];
 
-    for (let i = cid_test.length - 1; i >= 0; i--) {
-      if (power[cid_test[i][0]] < remainder && cid_test[i][1] > 0) {
-        cid_test[i][1] = Number(
-          (cid_test[i][1] - power[cid_test[i][0]]).toFixed(2)
-        );
-        amountPaid[cid_test[i][0]] += power[cid_test[i][0]];
-        return returnChange(
-          Number((cashPaid - power[cid_test[i][0]]).toFixed(2))
-        );
+      if (power[note] < change && noteAmount > 0) {
+        noteAmount = Number((noteAmount - power[note]).toFixed(2));
+        cid_temp[i][1] = noteAmount;
+        amountOfNoteReturned[note] += power[note];
+        change -= power[note];
+
+        const changeFixed = Number(change.toFixed(2));
+        return canReturnChange(changeFixed) || checkForChange(changeFixed);
       }
     }
-
-    result.stat = "Insufficient_funds";
-    result.amountPaid = {
-      PENNY: 0,
-      NICKEL: 0,
-      DIME: 0,
-      QUARTER: 0,
-      ONE: 0,
-      FIVE: 0,
-      TEN: 0,
-      TWENTY: 0,
-      "ONE HUNDRED": 0,
-    };
-
-    return result;
   };
 
-  if (cashPaid <= price) {
-    return "";
-  } else {
-    return returnChange(cashPaid);
+  return canReturnChange(change) || checkForChange(Number(change.toFixed(2)));
+};
+
+
+
+const update_cid = () => {
+  listOfNotes.innerHTML = `<p><strong>Change in drawer:</strong></p>`;
+  for (let i = 0; i < cid.length; i++) {
+    let note = cid[i][0];
+    let noteAmount = cid[i][1];
+    listOfNotes.innerHTML += `<p id=${note}>${note}: $${noteAmount}</p>`;
   }
 };
 
-const priceElement = document.getElementById("price");
-const output = document.getElementById("output");
-const listOfNotes = document.getElementById("list-of-notes");
-const cash = document.getElementById("cash-value");
-const purchaseButton = document.getElementById("purchase");
-priceElement.innerText = price;
+update_cid();
 
-for (let i = 0; i < cid.length; i++) {
-  listOfNotes.innerHTML += `<p id=${cid[i][0]}>${cid[i][0]}: $${cid[i][1]}</p>`;
-}
+const visulaize_changes = () => {
+  debugger;
+  {
+    let changeInfo = has_change(cash.value);
+    output.innerHTML = "";
+    if (cash.value == price) {
+      output.innerHTML += `<p >No change due - customer paid with exact cash</p>`;
+      cash.value = "";
+    } else if (Number(cash.value) < price && cash.value !== "") {
+      alert("Customer does not have enough money to purchase the item");
+      cash.value = "";
+    } else if (cash.value === "") {
+      cash.value = "";
+      return;
+    } else if (changeInfo) {
+      const { status, amountOfNoteReturned } = changeInfo;
+      output.innerHTML += `<p >STATUS: ${status}</p>`;
 
-purchaseButton.addEventListener("click", () => {
-  let result = changeStatus(cash.value);
-  output.innerHTML = "";
-  if (result) {
-    const { stat, amountPaid } = result;
-    output.innerHTML += `<p >STATUS: ${stat}</p>`;
-
-    for (let note in amountPaid) {
-      if (amountPaid[note]) {
-        output.innerHTML += `<p> ${note}: $${Number(amountPaid[note].toFixed(2))}</p>`;
+      for (let note in amountOfNoteReturned) {
+        if (amountOfNoteReturned[note]) {
+          output.innerHTML += `<p> ${note}: $${Number(amountOfNoteReturned[note].toFixed(2))}</p>`;
+        }
       }
+      update_cid();
+      cash.value = "";
+    } else if (!changeInfo) {
+      output.innerHTML += `<p >STATUS: INSUFFICIENT_FUNDS</p>`;
+      cash.value = "";
     }
-    listOfNotes.innerHTML = `<p><strong>Change in drawer:</strong></p>`;
-    for (let i = 0; i < cid.length; i++) {
-      listOfNotes.innerHTML += `<p id=${cid[i][0]}>${cid[i][0]}: $${cid[i][1]}</p>`;
-    }
-    cash.value = "";
-  } else if (cash.value == price) {
-    output.innerHTML += `<p >No change due - customer paid with exact cash</p>`;
-    cash.value = "";
-  } else if (Number(cash.value) < price && cash.value !== "") {
-    alert("Customer does not have enough money to purchase the item");
-    cash.value = "";
-  } else if ((cash.value = "")) {
-    return;
+  }
+};
+
+purchaseButton.addEventListener("click", () => visulaize_changes());
+
+cash.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    visulaize_changes();
   }
 });
